b76c450edc6c41740bb9e201c158ae965fef8bbd
Aditya <holaaditya123@gmail.com>
2024-07-30 18:08:28 +0530

    Add lazy loading for images and use Loader component
    Improve page load performance by implementing lazy loading for images. Also, replace the fallback loading message with the Loader component for a more consistent and visually appealing loading experience.

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Loader from "@/components/Loader";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
@@ -9,9 +10,7 @@ import type { ReactNode } from "react";
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
-      <ClientSideSuspense fallback={<div>Loading…</div>}>
-        {children}
-      </ClientSideSuspense>
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
 };
