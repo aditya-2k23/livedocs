ae0ad876aaeee36f8cf0fff8c64ed4095b59bbcf
Aditya <holaaditya123@gmail.com>
2024-08-01 17:51:13 +0530

    feat: Add FloatingThreads component for displaying floating threads
    This commit adds the FloatingThreads component to the Editor.tsx file. The component is imported from the @liveblocks/react/suspense package and is used to display floating threads in the editor. This enhancement improves the functionality of the application by allowing users to view and interact with threads in a floating window while editing the content.

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -13,6 +13,7 @@ import React from "react";
 
 import {
   FloatingComposer,
+  FloatingThreads,
   liveblocksConfig,
   LiveblocksPlugin,
   useEditorStatus,
@@ -20,6 +21,7 @@ import {
 import Loader from "../Loader";
 
 import FloatingToolbarPlugin from "@/components/editor/plugins/FloatingToolbarPlugin";
+import { useThreads } from "@liveblocks/react/suspense";
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -37,6 +39,7 @@ export function Editor({
   currentUserType: UserType;
 }) {
   const status = useEditorStatus();
+  const { threads } = useThreads();
 
   const initialConfig = liveblocksConfig({
     namespace: "Editor",
@@ -75,7 +78,10 @@ export function Editor({
             </div>
           )}
 
-          <LiveblocksPlugin></LiveblocksPlugin>
+          <LiveblocksPlugin>
+            <FloatingComposer className="w-[350px]" />
+            <FloatingThreads threads={threads} />
+          </LiveblocksPlugin>
         </div>
       </div>
     </LexicalComposer>
