32531139a46ba2a8efad08ee3c2bc961fdee3861
Aditya <holaaditya123@gmail.com>
2024-08-02 23:26:45 +0530

    feat: Add UserTypeSelector and Collaborator components for sharing and managing document collaborators

--- components/ShareModal.tsx
+++ components/ShareModal.tsx
@@ -15,6 +15,8 @@ import { Button } from "./ui/button";
 import Image from "next/image";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
+import UserTypeSelector from "./UserTypeSelector";
+import Collaborator from "./Collaborator";
 
 const ShareModal = ({
   roomId,
@@ -69,7 +71,31 @@ const ShareModal = ({
               onChange={(e) => setEmail(e.target.value)}
               className="share-input"
             />
+
+            <UserTypeSelector userType={userType} setUserType={setUserType} />
           </div>
+          <Button
+            type="submit"
+            onClick={shareDocumentHandler}
+            className="gradient-blue flex h-full gap-1 px-5"
+          >
+            {loading ? "Sharing..." : "Invite"}
+          </Button>
+        </div>
+
+        <div className="my-2 space-y-2">
+          <ul className="flex flex-col">
+            {collaborators.map((collaborator) => (
+              <Collaborator
+                key={collaborator.id}
+                roomId={roomId}
+                creatorId={creatorId}
+                email={collaborator.email}
+                collaborator={collaborator}
+                user={user.info}
+              />
+            ))}
+          </ul>
         </div>
       </DialogContent>
     </Dialog>
